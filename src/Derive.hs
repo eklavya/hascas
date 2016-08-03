06cfb42d545f34b90b38aafc79ba26b1dbd092e8
Saurabh Rawat <saurabh.rawat90@gmail.com>
2016-08-03 23:36:18 +0530

    refactor derivation

--- src/Derive.hs
+++ src/Derive.hs
@@ -13,20 +13,21 @@ import           Data.ByteString.Lazy as DBL (ByteString)
 import           Data.Char            (toLower)
 import           Data.Map.Strict      (Map, lookup)
 import           Language.Haskell.TH
+import           Prelude              hiding (lookup)
 
 
 genNames :: Int -> Q [Name]
 genNames n = replicateM n $ newName "x"
 
 statements :: [Name] -> [(Name, Strict, Type)] -> Name -> [StmtQ]
-statements names args m = fmap (\(n, (name, _, tpe)) -> do
+statements names args r = fmap (\(n, (name, _, tpe)) -> do
                             bytesName <- newName "b"
                             bindS
                               (varP n)
                               (infixE
-                              (Just (infixE (Just (appE (varE $ mkName "runGet") (sigE (varE $ mkName "get") (appT (conT $ mkName "Get") (pure tpe))))) (varE $ mkName ".") (Just (lamE [tupP [wildP,wildP,wildP, conP (mkName "CQL.Bytes") [varP bytesName]]] (varE bytesName)))))
+                              (Just (infixE (Just (appE (varE 'runGet) (sigE (varE 'get) (appT (conT ''Get) (pure tpe))))) (varE $ mkName ".") (Just (lamE [tupP [wildP,wildP,wildP, conP (mkName "CQL.Bytes") [varP bytesName]]] (varE bytesName)))))
                               (varE $ mkName "<$>")
-                              (Just (appE (appE (varE $ mkName "Data.Map.Strict.lookup") (appE (conE $ mkName "CQLString") (litE (StringL (fmap toLower (nameBase name)))))) (varE m))))
+                              (Just (appE (appE (varE 'lookup) (appE (conE 'CQLString) (litE (StringL (fmap toLower (nameBase name)))))) (varE r))))
                           ) (names `zip` args)
 
 rtrnStmt names conName = [noBindS (appE (varE $ mkName "return") (applyRec (appE (conE conName) (varE $ head names)) (tail names)) )]
@@ -35,10 +36,11 @@ applyRec = foldl (\ex nm -> appE ex (varE nm))
 
 deriveBuildRec a = do
   TyConI (DataD _ cName _ constructors _) <- reify a
-  let (RecC conName args) = head constructors
-  names <- genNames $ length args
-  d <- [d| instance BuildRec $(conT a) where
-            fromRow m = $(doE (statements names args (mkName "m") ++ rtrnStmt names conName)) |]
-  let    [InstanceD [] (AppT fromRowt (ConT _T1)) [FunD fromRowf _text]] = d
-         [Clause _ some thing] = _text
-  return [InstanceD [] (AppT fromRowt (ConT a)) [FunD fromRowf [Clause [VarP $ mkName "m"] some thing]]]
+  case head constructors of
+    (RecC conName args) -> do
+      names <- genNames $ length args
+      rowName <- newName "r"
+      [d| instance BuildRec $(conT a) where
+            fromRow = $(lamE [varP rowName] (doE (statements names args rowName ++ rtrnStmt names conName))) |]
+
+    _ -> fail "deriveBuildRec: Only simple records supported, is the type you are deriving for, a record type?"
